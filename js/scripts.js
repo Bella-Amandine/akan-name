f872599ce4c63df783558828befbafe7e1866047
Bella Amandine <bellamandy45@gmail.com>
2020-09-26 22:07:41 +0200

    Add functions to get gender and dob from  user

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+function getGender(){
+    var gender = document.getElementById("gender").value;
+    return gender;
+}
+function getDob() {
+    var dob = document.getElementById("dob").value;
+    return dob;
+}
+
