c295f6f22bf16ddc05586449b0c4f05311aea4e6
Bella Amandine <bellamandy45@gmail.com>
2020-09-26 22:50:36 +0200

    Add function to calculate day of the week

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,19 @@ function getDob() {
     return dob;
 }
 
+document.getElementById("submitBtn").addEventListener('click', function(){
+    var gender = getGender();
+    var dob = getDob(); 
+    calculateDate(dob);
+})
+
+function calculateDate(dob) {
+    
+    var cc = dob.substring(0,2);
+    var yy = dob.substring(2,4);
+    var mm = dob.substring(5,7);
+    var dd = dob.substring(8,10);
+
+    var dayOfTheWeek = (((cc/4)-2*CC-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd ) % 7;
+    return dayOfTheWeek;
+}
