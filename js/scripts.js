211d2a561c9de65207df01de524434e676c20d75
Bella Amandine <bellamandy45@gmail.com>
2020-09-28 00:57:06 +0200

    Parse string date into integers when calculating day

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,24 @@ function getDob() {
 document.getElementById("submitBtn").addEventListener('click', function(){
     var gender = getGender();
     var dob = getDob(); 
-    calculateDate(dob);
+    var day = calculateDate(dob);
+    displayAkanName(day, gender);
+    alert(day);
 })
 
 function calculateDate(dob) {
-    
-    var cc = dob.substring(0,2);
-    var yy = dob.substring(2,4);
-    var mm = dob.substring(5,7);
-    var dd = dob.substring(8,10);
+    var cc = parseInt(dob.substring(0,2));
+    var yy = parseInt(dob.substring(2,4));
+    var mm = parseInt(dob.substring(5,7));
+    var dd = parseInt(dob.substring(8,10));
 
-    var dayOfTheWeek = (((cc/4)-2*CC-1) + ((5*yy/4)) + ((26*(mm+1)/10)) + dd ) % 7;
+    var dayOfTheWeek = (((cc/4)-2*cc-1) + (5*yy/4) + (26*(mm+1)/10) + dd ) % 7;
     return dayOfTheWeek;
+}
+
+function displayAkanName(day, gender){
+    if(day === 0 && gender === "female"){
+       alert("Your Akan Name is : Akosua")
+       // document.getElementById("akan-name").innerHTML = "Your akan name is : Akosua"
+    }
 }
